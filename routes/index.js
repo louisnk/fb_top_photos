df22dd0b747bd8773bec994ce8b6b03508e0dbce
Louis Kinley <louis.kinley@gmail.com>
2014-09-28 21:23:47 -0700

    template updated

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,32 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'FB Top Photo Finder' });
+  res.render('index', 
+    { title: {
+        title: 'facebook top photo rankings',
+        icon: 'icon icon-menu'
+      },
+      fb: {
+        title: 'Facebook Top Photo Rankings',
+        text: 'Please log into facebook to see the ranking of your 25 most recent photos uploaded.',
+        start: 'icomoon icon-lab',
+        buttons: [
+          { text: "Like", attribute: "like" },
+          { text: "Comment", attribute: "comment" }
+        ],
+        flows: [
+          { text: "Log in with Facebook", 
+            icon: 'icomoon icon-facebook'
+          },
+          {
+            text: "Wait for the magic",
+            icon: 'icomoon icon-lightning'
+          },
+          {
+            text: "Profit",
+            icon: 'icomoon icon-user2'
+          }
+        ]
+      }
+   });
 };
